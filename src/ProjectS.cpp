d1b82b5377e665e1b1d1d93905673388ecc7335d
q1584428087@163.com <q1584428087@163.com>
2019-12-11 21:41:04 +0800

    Mgy部分更新完成

--- src/ProjectS.cpp
+++ src/ProjectS.cpp
@@ -4,26 +4,9 @@
 #include<conio.h>
 #include<time.h>
 #include<Windows.h>
-#include<mmsystem.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include<iostream>
-#include <fcntl.h>
-#include <process.h> 
-#include <bits/stdc++.h>
-#include <sys/stat.h>
-#pragma comment(lib, "WINMM.LIB")
+
+
 #define _CRT_SECURE_NO_WARNINGS 1
-//色彩库
-#define Red SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_INTENSITY);
-#define Blue SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_BLUE|FOREGROUND_INTENSITY);
-#define Green SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_GREEN|FOREGROUND_INTENSITY);
-#define White SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-#define Yellow SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_RED|FOREGROUND_GREEN|FOREGROUND_INTENSITY);
-#define Pink SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_RED|FOREGROUND_BLUE);
-#define Cyan SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),FOREGROUND_INTENSITY|FOREGROUND_GREEN|FOREGROUND_BLUE);
-//使用后如要恢复原色请使用以下宏定义
-#define YuanSe SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE));
 
 //字母结构体
 typedef struct Word
@@ -40,7 +23,6 @@ Word* pCurWord = pFirstWord;
 //绘制游戏信息,卡关(实现),分数(全局);
 void DrawGameInfo(int level);
 
-
 //设置光标位置
 void goto_xy(int x, int y);
 
@@ -51,13 +33,22 @@ int StartGame(int iLevel);
 void DrawWinShell(int iScore, int iNextLevel);
 bool DrawLoseShell(int iScore);
 
-void DeleteWord(Word *pTmp)
+
+void DeleteWord(Word* pTmp)
 {
 	//删去字母
 	goto_xy(pFirstWord->iPos, pFirstWord->iPosY);
 	printf(" ");
+
 	pFirstWord = pTmp->pNext;
 }
+
+char* GetStartText();
+char* GetGoText();
+char* GetGameOverText();
+char* GetLevelText();
+
+
 
 HANDLE hOut;
 CONSOLE_SCREEN_BUFFER_INFO bInfo;
@@ -68,12 +59,12 @@ int CONSOLE_WIDTH;
 int CONSOLE_HEIGHT;
 
 //正确,错误的分数
-int iRightScore = 100;
-int iErrorScore = 90;
+int iRightScore = 10;
+int iErrorScore = 5;
 
 //胜利/失败 次数
 int iWinTimes = 10;
-int iLoseTimes = 1112;
+int iLoseTimes = 10;
 
 //int iSuccessScore = iRightScore* iWinTimes;
 //int iLoseScore = iLoseTime * iErrorScore;
@@ -91,23 +82,44 @@ int iDeadline = 0;
 char iWordPos[40];
 
 //初始卡关时间(毫秒)
-int iInitialTime = 2000;
+int iInitialTime = 1000;
 int iNewAlphaTime;
+int iFallTime = 150;
+
 
 //时间相关
 time_t curtime;
 clock_t start;
 clock_t now;
 
-int main(int argc, char* argv[])
+int main()
 {
 	//初始化
 	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
-	GetConsoleScreenBufferInfo(hOut, &bInfo);
-
+	
+	//清屏
 	system("cls");
 	//设置控制台标题
 	SetConsoleTitle(TEXT("TYPE GAME"));
+
+	//设置控制台大小
+	system("mode con cols=91 lines=29  ");
+	//锁死控制台边框大小，防止因为拉伸控制台边框大小而导致的错误
+	//（同时此处建议可以扩大控制台的大小
+	HWND hWnd = GetConsoleWindow(); //获得cmd窗口句柄
+	RECT rc;
+	GetWindowRect(hWnd, &rc); //获得cmd窗口对应矩形
+	//改变cmd窗口风格
+	SetWindowLongPtr(hWnd,
+		GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_THICKFRAME & ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX);
+	SetWindowPos(hWnd,
+		NULL,
+		rc.left,
+		rc.top,
+		rc.right - rc.left, rc.bottom - rc.top,
+		NULL);
+
+
 
 	//隐藏光标
 	CONSOLE_CURSOR_INFO cInfo;
@@ -118,8 +130,8 @@ int main(int argc, char* argv[])
 
 
 	//获取控制台宽度
+	GetConsoleScreenBufferInfo(hOut, &bInfo);
 	CONSOLE_WIDTH = bInfo.dwSize.X;
-
 	//控制台高度,暂时无法获取
 	CONSOLE_HEIGHT = 28;
 
@@ -131,42 +143,30 @@ int main(int argc, char* argv[])
 	srand(curtime);
 
 	//游戏欢迎界面
-	for (size_t i = 0; i < 100; i++)
-	{
-		printf("\n");
-	}
-	goto_xy(0, 0);
 
+	goto_xy(0, 3);
+	printf(GetStartText());
+	goto_xy(34, 17);
+	printf("欢迎来到打字游戏!\n\n\n");
 
-	White		//白色输出
-	printf("Welcome To Type Game!\n\n\n");
-	Red			//红色输出
-	printf("Please Set The Game Shell Width By Scrolling The Console!!!\n\n\n");
-	Green		//绿色输出
+	goto_xy(33, 22);
+	printf("按下任何键来开始游戏\n");
+	goto_xy(30, 23);
+
 	printf("Type Any Key To Start Game\n\n\n");
-	White		//白色输出
 
 
-	getchar();
+
 	
-
-	//锁死控制台边框大小，防止因为拉伸控制台边框大小而导致的错误
-	//（同时此处建议可以扩大控制台的大小
-	HWND hWnd = GetConsoleWindow(); //获得cmd窗口句柄
-	RECT rc;
-	GetWindowRect(hWnd, &rc); //获得cmd窗口对应矩形
-
-	//改变cmd窗口风格
-	SetWindowLongPtr(hWnd,
-		GWL_STYLE, GetWindowLong(hWnd, GWL_STYLE) & ~WS_THICKFRAME & ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX);
-	SetWindowPos(hWnd,
-		NULL,
-		rc.left,
-		rc.top,
-		rc.right - rc.left, rc.bottom - rc.top,
-		NULL);
-		
+	_getch();
 	
+	SetConsoleCursorInfo(hOut, &cInfo);
+
+	//debug
+	DrawWinShell(1, 1);
+
+	_getch();
+	//debug
 
 
 	//游戏开始
@@ -197,12 +197,18 @@ int main(int argc, char* argv[])
 	printf("Thanks for playing!");
 }
 
-
 int StartGame(int iLevel)
 {
 	system("cls");
-	//设置产生字母的间隔时间s
+
+	//设置产生字母的间隔时间
 	iNewAlphaTime = (int)iInitialTime * (pow(0.8, iLevel));
+	if (iNewAlphaTime < iFallTime)
+	{
+		iFallTime = iNewAlphaTime * 0.5;
+	}
+
+
 
 	//读取控制台宽度
 	GetConsoleScreenBufferInfo(hOut, &bInfo);
@@ -215,7 +221,7 @@ int StartGame(int iLevel)
 	start = clock();
 	int iTimeInDrop = 0;
 	int iTimeInNewWord = 0;
-	
+
 
 	//初始化字母链表
 	pFirstWord = (Word*)malloc(sizeof(Word));
@@ -227,30 +233,42 @@ int StartGame(int iLevel)
 	iMission = iRightScore * iWinTimes + iScoreOnNewLevel;
 	iDeadline = iScoreOnNewLevel - iErrorScore * iLoseTimes;
 
-
 	//若失败分数小于0,则设为0(防止出现负分数)
 	if (iDeadline < 0) iDeadline = 0;
 
 
 
-	//上次系统生成的字母
-	char cLastAlpha = 0;
-
-
 	//上次的字母是否已经敲入
 	bool bHasTyped = false;
 
-
-	//绘制游戏界面	
-
-
+	//---------------------------------------
+	//绘制游戏界面
 	DrawGameInfo(iLevel);
 	//画底部
 	goto_xy(0, CONSOLE_HEIGHT);
-	for (size_t i = 0; i < CONSOLE_WIDTH; i++)
+	// ╦ ╩ ╝ ╔ ╗ ╚ ═ ║ 
+	/*
+			╔═╦═╗
+			║ ║ ║
+			╠═╬═╣
+			║ ║ ║
+			╚═╩═╝
+	*/
+	printf("╚");
+	for (size_t i = 0; i < CONSOLE_WIDTH - 3; i++)
 	{
-		printf("=");
+		printf("═");
 	}
+	printf("╝");
+	//输出左右边框
+	for (size_t i = 0; i < CONSOLE_HEIGHT; i++)
+	{
+		goto_xy(0, i);
+		printf("║");
+		goto_xy(CONSOLE_WIDTH-2, i);
+		printf("║");
+	}
+
 
 	//开始生成
 	while (1) {
@@ -269,15 +287,13 @@ int StartGame(int iLevel)
 			//若当前的字母没有敲入
 			//if (!bHasTyped)
 			//若当前屏幕有字母
-			if (pFirstWord->cWord>0)
+			if (pFirstWord->cWord > 0)
 			{
 
 				//正确输入
 				if (cTypedAplha == pFirstWord->cWord || cTypedAplha == pFirstWord->cWord + ('a' - 'A')) {
 					iScore += iRightScore;
 					DeleteWord(pFirstWord);
-					PlaySound(TEXT("Ding.wav"), NULL, SND_ASYNC | SND_NODEFAULT);
-					//PlaySound(TEXT("Ding.wav"), 0, SND_FILENAME);
 					//此处可增加反馈:OnInputCorrectly()
 					//1.正确输入的字母变绿
 					//2.正确输入的音效!
@@ -300,13 +316,11 @@ int StartGame(int iLevel)
 		}
 
 		//整体下落
-		int iDropTime = 500;
+
 		int iGameInfoLength = 5;
 		int iGameBufferHeight = CONSOLE_HEIGHT - iGameInfoLength - 1;
 
-
-		//生成新的字母
-		if ((now - start) / (CLOCKS_PER_SEC / 1000) - iTimeInDrop >= iDropTime) {
+		if ((now - start) / (CLOCKS_PER_SEC / 1000) - iTimeInDrop >= iFallTime) {
 
 			//更新计时器
 			iTimeInDrop = (now - start) / (CLOCKS_PER_SEC / 1000);
@@ -335,7 +349,7 @@ int StartGame(int iLevel)
 					//printf(" ");
 					//pFirstWord = pTmp->pNext;
 					//pTmp = pTmp->pNext;
-					
+
 				}
 
 				//将字母下移一行
@@ -361,19 +375,17 @@ int StartGame(int iLevel)
 
 
 
-
-
 			//产生新字母
 			char cRandChar = rand() % 26 + 'A';
 			pCurWord->cWord = cRandChar;
 
 			//iPos = 随机字母的位置
-			int iPos = rand() % CONSOLE_WIDTH;
+			int iPos = rand() % (CONSOLE_WIDTH-6) + 3;
 			goto_xy(iPos, iGameInfoLength + 1);
 			printf("%c", cRandChar);
 
 			pCurWord->iPos = iPos;
-			pCurWord->iPosY = iGameInfoLength+1;
+			pCurWord->iPosY = iGameInfoLength + 1;
 			pCurWord->cWord = cRandChar;
 			pCurWord->pNext = (Word*)malloc(sizeof(Word));
 			pCurWord = pCurWord->pNext;
@@ -410,7 +422,6 @@ int StartGame(int iLevel)
 //		 ║										║
 //		 ╚══════════════════════════════════════╝
 //
-
 void DrawGameInfo(int level) {
 
 	//获取当前光标位置(备份)
@@ -421,45 +432,48 @@ void DrawGameInfo(int level) {
 	goto_xy(0, 0);
 	//goto_xy(0, bInfo.dwCursorPosition.Y > CONSOLE_HEIGHT ? bInfo.dwCursorPosition.Y - CONSOLE_HEIGHT + 2 : 0);
 
-	//开始绘制
-	//制表符: ╦ ╩ ╝ ╔ ╗ ╚ ═ ║ 
-	//不要用putchar 会不显示
+	//开始绘制       
+	//制表符: ╦ ╩ ╝ ╔ ╗ ╚ ═ ║ ╠   ╬ ╠   ╦ ╬ ╩   ╣ 
+/*
+			╔═╦═╗
+			║ ║ ║
+			╠═╬═╣
+			║ ║ ║
+			╚═╩═╝
+*/
+//不要用putchar 会不显示
 	printf("╔");
-	for (size_t i = 1; i < CONSOLE_WIDTH - 1; i++)
+	for (size_t i = 1; i < CONSOLE_WIDTH - 2; i++)
 	{
 		printf("═");
 	}
 	printf("╗");
 
-	//绘制空行
-	goto_xy(0, bInfo.dwCursorPosition.Y > CONSOLE_HEIGHT ? bInfo.dwCursorPosition.Y - CONSOLE_HEIGHT + 2 : 1);
-	printf("%120c", ' ');
 
 	//开始绘制游戏信息
 	goto_xy(0, bInfo.dwCursorPosition.Y > CONSOLE_HEIGHT ? bInfo.dwCursorPosition.Y - CONSOLE_HEIGHT + 3 : 2);
-	//此处可能会影响后面的字母
-	//输出卡关,分数,速度,胜利/失败分数,时间
-	printf("LEVEL:%3d SCORE:%5d SPEED:%4dms Mission:%4d Deadline:%4d %100c",
-		level, iScore, iNewAlphaTime, iMission, iDeadline, ' ');
 
-	goto_xy(0, bInfo.dwCursorPosition.Y > CONSOLE_HEIGHT ? bInfo.dwCursorPosition.Y - CONSOLE_HEIGHT + 4 : 3);
-	printf("%120c", ' ');
+	//输出卡关,分数,速度,胜利/失败分数,时间
+	goto_xy(10, 2);
+	printf("LEVEL:%3d SPEED:%4dms SCORE:%5d Mission:%4d Deadline:%4d",
+		level, iNewAlphaTime, iScore,iMission, iDeadline);
+
+
 
 	goto_xy(0, bInfo.dwCursorPosition.Y > CONSOLE_HEIGHT ? bInfo.dwCursorPosition.Y - CONSOLE_HEIGHT + 5 : 4);
 
 
-	printf("╚");
-	for (size_t i = 1; i < CONSOLE_WIDTH - 1; i++)
+	printf("╠");
+	for (size_t i = 1; i < CONSOLE_WIDTH - 2; i++)
 	{
 		printf("═");
 	}
-	printf("╝");
+	printf("╣");
 
 	//恢复光标信息
 	goto_xy(bInfo.dwCursorPosition.X, bInfo.dwCursorPosition.Y - 1);
 
 }
-
 
 
 //将光标移动到(x,y)
@@ -469,15 +483,27 @@ void goto_xy(int x, int y)
 	SetConsoleCursorPosition(hOut, pos);
 }
 
-
-
 //绘制卡关胜利的界面
 void DrawWinShell(int iScore, int iNextLevel)
 {
 	system("cls");
-	printf("Congratulation!\n");
-	printf("Your Score:%d\n\n\n", iScore);
-	printf("Are you ready for next level %d?\nPRESS Y TO GO", iNextLevel);
+
+	goto_xy(0, 2);
+	char* a = GetLevelText();
+	printf(a);
+	int line = 10;
+	int col = 33;
+	goto_xy(col, line++);
+	printf("╔══════════════════╗\n"); goto_xy(col, line++);
+	printf("║                  ║\n"); goto_xy(col, line++);
+	printf("║    Your Score    ║\n"); goto_xy(col, line++);
+	printf("║      %6d      ║\n",iScore); goto_xy(col, line++);
+	printf("║                  ║\n"); goto_xy(col, line++);
+	printf("╚══════════════════╝\n\n", iScore); goto_xy(col-3, line+5);
+
+	printf("Are you ready for LEVEL - %d?\n\n", iNextLevel);
+
+	puts(GetGoText());
 
 	//监测玩家抉择
 
@@ -495,7 +521,6 @@ void DrawWinShell(int iScore, int iNextLevel)
 
 
 
-
 //绘制卡关失败的界面
 //选择是否重新尝试卡关
 //若分数小于0 直接失败
@@ -507,8 +532,8 @@ bool DrawLoseShell(int iScore)
 
 	system("cls");
 
-	printf("Oh! You Failed!\n");
-	printf("Do you want to play this level again?\n");
+	printf(GetGameOverText());
+	printf("\n\n\nDo you want to play this level again?\n");
 	printf("Press Y for Yes or press N for Give Up\n");
 
 	//监测玩家的选择
@@ -524,4 +549,45 @@ bool DrawLoseShell(int iScore)
 			return false;
 		}
 	}
+}
+
+
+char* GetLevelText()
+{
+	char* str = (char*)calloc(1000,1);
+	FILE* file;
+	fopen_s(&file, "resource/LEVEL.txt", "r");
+	fread(str, 666, 1, file);
+	str[666] = 0;
+	return str;
+}
+char* GetStartText()
+{
+	char* str = (char*)calloc(1000,1);
+	FILE* file;
+	fopen_s(&file, "resource/TypeGame.txt", "r");
+	fread(str, 555, 1, file);
+	fclose(file);
+	str[555] = 0;
+	return str;
+}
+char* GetGoText()
+{
+	char* str = (char*)calloc(1000,1);
+	FILE* file;
+	fopen_s(&file, "resource/Go_Next.txt", "r");
+	fread(str, 555, 1, file);
+	fclose(file);
+	str[555] = 0;
+	return str;
+}
+char* GetGameOverText()
+{
+	char* str = (char*)calloc(1000,1);
+	FILE* file;
+	fopen_s(&file, "resource/Gameover.txt", "r");
+	fread(str, 555, 1, file);
+	fclose(file);
+	str[555] = 0;
+	return str;
 }
